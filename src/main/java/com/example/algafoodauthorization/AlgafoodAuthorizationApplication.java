cdc24f1f30e61a6da579ad8eb5f10c4742dab8a3
Diego Mucheniski <mucheniski@gmail.com>
2021-06-18 08:40:14 -0300

    22.8. Criando o projeto do Authorization Server com Spring Security OAuth2

---
+++ src/main/java/com/example/algafoodauthorization/AlgafoodAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.algafoodauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodAuthorizationApplication.class, args);
+	}
+
+}
