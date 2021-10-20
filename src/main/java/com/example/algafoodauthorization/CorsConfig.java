bd2a98986528a59db17099ba1a1a6f48e3096a2c
Diego Mucheniski <mucheniski@gmail.com>
2021-10-20 16:14:06 -0300

    22.19. Testando o fluxo Authorization Code com um client JavaScript

---
+++ src/main/java/com/example/algafoodauthorization/CorsConfig.java
@@ -0,0 +1,38 @@
+package com.example.algafoodauthorization;
+
+import java.util.Collections;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/*
+* Classe criada para habilitar o CORS no authorization, evitando o erro 401 quando a chamada é feita
+* por um front externo, como por exemplo uma aplicação javascript.
+* */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/oauth/token", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>();
+        bean.setFilter(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return bean;
+    }
+
+}
